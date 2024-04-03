5d93d2706229d5911cc926bd400f02499c8a2da8
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2024-04-03 07:11:03 +0530

    Header Component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.compnent';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
